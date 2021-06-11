ad63be3c2df4e6098f0b0fa69253ed89438a8a0e
dbravin <davide.bravin@optionfactory.net>
2021-06-11 12:54:09 +0200

    :enh: support for negative number literals

--- src/test/java/net/optionfactory/pebbel/ExpressionEvaluatorTest.java
+++ src/test/java/net/optionfactory/pebbel/ExpressionEvaluatorTest.java
@@ -2,6 +2,7 @@ package net.optionfactory.pebbel;
 
 import java.io.StringReader;
 import net.optionfactory.pebbel.ast.BooleanExpression;
+import net.optionfactory.pebbel.ast.NumberExpression;
 import net.optionfactory.pebbel.execution.ExpressionEvaluator;
 import net.optionfactory.pebbel.parsing.JavaccParser;
 import net.optionfactory.pebbel.parsing.ParseException;
@@ -15,6 +16,23 @@ public class ExpressionEvaluatorTest {
         final JavaccParser parser = new JavaccParser(new StringReader(text));
         final BooleanExpression expression = parser.booleanExpression();
         return ce.visit(expression, BindingsMother.SYMBOLS);
+    }
+
+    private double evalNumber(String text) throws ParseException {
+        final ExpressionEvaluator<Object, Object> ce = new ExpressionEvaluator<>();
+        final JavaccParser parser = new JavaccParser(new StringReader(text));
+        final NumberExpression expression = parser.numberExpression();
+        return ce.visit(expression, BindingsMother.SYMBOLS);
+    }
+
+    @Test
+    public void canEvaluateNumberLiteral() throws ParseException {
+        Assert.assertEquals(1d, evalNumber("1"), 0.000001);
+    }
+
+    @Test
+    public void canEvaluateNegativeNumberLiteral() throws ParseException {
+        Assert.assertEquals(-1d, evalNumber("-1"), 0.000001);
     }
 
     @Test
